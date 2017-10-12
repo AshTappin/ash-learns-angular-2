7df31760254969e1cddf888b225bc29545b5dc13
Ash Tappin <ashtappin1592@hotmail.com>
2017-10-12 15:37:23 +0100

    Beginning to add HTML module and styled the heroes page slightly more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import {HeroService} from './hero.service';
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpModule} from '@angular/http';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule],
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)],
   declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
   providers:
     [HeroService],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero/hero.class';
-import {HEROES} from './mock-heroes';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) {
   }
 
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+
+  }
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    return this.http.get(`${this.heroesUrl}/${id}`)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
   }
 }
 
